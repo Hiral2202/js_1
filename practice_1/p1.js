bce116745a2736ccc004d3699c7aba3ce843b346
Hiral2202 <hiralkattawar@gmail.com>
2025-07-31 14:18:52 +0530

    1st js file

---
+++ practice_1/p1.js
@@ -0,0 +1,3 @@
+console.log("This is my first practice file in JavaScript.");
+console.log("I am learning how to write JavaScript code.");
+console.log("This file is located in the practice_1 directory.");
